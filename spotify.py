5f9b00396256f633d1bd3921c5c41d838a4094ea
John Gioffre <john@Johns-MacBook-Pro.local>
2019-12-08 18:10:02 -0500

    finished code for spotify playlist data. one table

--- spotify.py
+++ spotify.py
@@ -1,1 +1,28 @@
+import spotipy as sp
+from spotipy.oauth2 import SpotifyClientCredentials
+import sqlite3
+import os
 
+db_name = 'playlist_data.db'
+path = os.path.dirname(os.path.abspath(__file__))
+conn = sqlite3.connect(path+'/'+db_name)
+cur = conn.cursor()
+
+client_id='435610e6bc6741e8b60f11f6425b8397'
+client_secret='3884178bbf7d4004badd984c561543f2'
+
+client_credentials_managera = SpotifyClientCredentials(client_id,client_secret)
+spot = sp.Spotify(client_credentials_manager=client_credentials_managera)
+
+current_playlist = spot.user_playlist('bradgurdlinger', '6wyXbq1Zf8iF3OQWUAE0rS')
+
+songs = []
+
+for i in current_playlist['tracks']['items']:
+    songs.append((i['track']['name'], i['track']['duration_ms']/60000))
+
+cur.execute('CREATE TABLE IF NOT EXISTS GrillnChill (name TEXT, length REAL)')
+for x in songs[80:]:   
+    cur.execute('INSERT INTO GrillnChill (name, length) VALUES (?, ?)', (x[0], x[1]))
+conn.commit()
+cur.close()
