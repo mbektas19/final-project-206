a496279a7859cf9dba85a90d3cff9cd441111799
John Gioffre <john@Johns-MacBook-Pro.local>
2019-12-09 17:20:19 -0500

    finished one visualization and db join

--- spot_vis.py
+++ spot_vis.py
@@ -1,5 +1,6 @@
 import sqlite3
 import os
+import matplotlib.pyplot as plt
 
 db_name = 'playlist_data.db'
 path = os.path.dirname(os.path.abspath(__file__))
@@ -18,6 +19,28 @@ for i in cur:
 average_year = total/count
 
 print("Average Year: " + str(average_year))
+
+cur.execute('''SELECT Years.name, Years.year, Lengths.length
+FROM Years 
+LEFT JOIN Lengths 
+ON Years.name  = Lengths.name;''')
+
+joined_db = cur.fetchall()
+
+years = []
+lengths = []
+
+for i in joined_db:
+    years.append(i[1])
+    lengths.append(i[2])
+
+color = '#3399FF'
+plt.scatter(years, lengths, c=color)
+plt.title('Song Year vs. Length')
+plt.xlabel('Song Year')
+plt.ylabel('Song Length')
+plt.show()
+
 
 conn.commit()
 cur.close()
