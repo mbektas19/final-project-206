09a6c1961d6b50b98ee8b2f68e66248503bcf2a5
annisrodrigue <40401617+annisrodrigue@users.noreply.github.com>
2019-12-10 00:41:59 -0500

    Add files via upload

--- yelp.py
+++ yelp.py
@@ -1,1 +1,46 @@
+import requests
+import json
+import sqlite3
+import os
 
+db_name = 'yelp.db'
+path = os.path.dirname(os.path.abspath(__file__))
+conn = sqlite3.connect(path+'/'+db_name)
+cur = conn.cursor()
+
+# change param 'offset' in order to get next results
+# api returns 20 results
+api_key = 'OPUebJ4zTSF3JNrO0QGxfpOZve7u5LvWpOe5hDCEwfW3q-PwTHGAWD-ad6ySoyIjmwFh7vAINGu475mwGkQeXGy1oY-Ev0IIuNhZRMfeI_7-gS79xtTMejSiYQfvXXYx'
+headers = {'Authorization': 'Bearer %s' % api_key}
+params = {'location':'Ann Arbor', 'offset':120, 'limit': 20}
+url='https://api.yelp.com/v3/businesses/search'
+
+res = requests.get(url, headers = headers, params = params)
+data = json.loads(res.text)
+
+cur.execute('CREATE TABLE IF NOT EXISTS Ratings (Name TEXT PRIMARY KEY UNIQUE, Category TEXT, Rating REAL)')
+
+for i in data['businesses']:
+    name = i['name']
+    category = i['categories'][0]['title']
+    rating = i['rating']
+    try:
+        cur.execute('INSERT INTO Ratings (Name, Category, Rating) VALUES (?,?,?)', (name, category, rating))
+    except:
+        None
+
+cur.execute('CREATE TABLE IF NOT EXISTS Prices (Name TEXT PRIMARY KEY UNIQUE, Price TEXT)')
+
+for x in data['businesses']:
+    name = x['name']
+    try:
+        price = x['price']
+    except:
+        price = None
+    try:
+        cur.execute('INSERT INTO Prices (Name, Price) VALUES (?,?)', (name, price))
+    except:
+        None
+
+conn.commit()
+cur.close()
--- yelp_vis.py
+++ yelp_vis.py
@@ -1,1 +1,87 @@
+import sqlite3
+import os
+import matplotlib.pyplot as plt
+import numpy as np
 
+db_name = 'yelp.db'
+path = os.path.dirname(os.path.abspath(__file__))
+conn = sqlite3.connect(path+'/'+db_name)
+cur = conn.cursor()
+
+category_counts = {}
+rating_totals = {}
+price_totals = {}
+tuples = []
+
+cur.execute('SELECT Category, Rating FROM Ratings')
+cur.execute('''SELECT Ratings.Name, Ratings.Category, Ratings.Rating, Prices.Price
+FROM Ratings 
+LEFT JOIN Prices 
+ON Ratings.Name  = Prices.Name;''')
+
+joined_db = cur.fetchall()
+
+for i in joined_db:
+    if i[1] not in category_counts:
+        category_counts[i[1]] = 1
+        rating_totals[i[1]] = i[2]
+        try:
+            price_totals[i[1]] = len(i[3])
+        except:
+            None
+    else:
+        category_counts[i[1]] += 1
+        rating_totals[i[1]] += i[2]
+        try:
+            price_totals[i[1]] += len(i[3])
+        except:
+            None
+
+average_ratings = {}
+average_prices = {}
+for i in category_counts:
+    average_ratings[i] = (rating_totals[i]/category_counts[i])
+for i in category_counts:
+    try:
+        average_prices[i] = price_totals[i]/category_counts[i]
+    except:
+        None
+
+# f = open('yelp_ratings_and_prices.txt', 'w')
+
+# for i in average_ratings:
+#     f.write(i + " " + str(average_ratings[i]) + '\n')
+# for i in average_prices:
+#     f.write(i + " " + str(average_prices[i]) + '\n')
+# f.close()
+
+#plot 1
+
+categories = []
+totals = []
+ratings = []
+
+for i in category_counts:
+    categories.append(i)
+    totals.append(category_counts[i])
+    ratings.append(average_ratings[i])
+    try:
+        tuples.append((i, average_prices[i], average_ratings[i]))
+    except:
+        None
+# plt.bar(range(59), totals)
+# plt.xticks(y_pos, categories)
+# plt.show()
+
+#PLOT 2
+
+x = []
+y = []
+for i in tuples:
+    x.append(i[2])
+    y.append(i[1])
+plt.scatter(x,y,c='#8a66d9')
+plt.title('Scatterplot of Price vs. Rating in Ann Arbor Restaurants')
+plt.xlabel('Average Rating per Category')
+plt.ylabel('Average Price ($-\$$$)')
+plt.show()
